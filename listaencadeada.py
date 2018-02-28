3a6f0f2f1b1b22364cf43eb521371b068e91e7da
Luan Kleyton <36345746+kleytonluan@users.noreply.github.com>
2018-02-28 13:07:04 -0300

    Update listaencadeada.py

--- listaencadeada.py
+++ listaencadeada.py
@@ -1,25 +1,28 @@
 class No:
-  def __init__(self,valor):
-    self.valor = valor
-    self.proximo = None
+    def __init__(self,valor):
+        self.valor = valor
+        self.proximo = None
   
 class Lista:
-  def __init__(self):
-    self.inicio=None
-    
-   def adicionar(self,item):
-    temp = No(item)
-    temp.proximo = self.inicio
-    self.inicio = temp
-    
-   def mostrar(self):
-    if self.inicio == None:
-      print("Lista vazia")
-    else:
-      aux = self.inicio
-      while aux != None:
-        print(aux.valor)
-        aux = aux.proximo
+    def __init__(self):
+        self.inicio=None
+
+
+    def adicionar(self,item):
+        temp = No(item)
+        temp.proximo = self.inicio
+        self.inicio = temp
+
+
+    def mostrar(self):
+
+        if self.inicio == None:
+              print("Lista vazia")
+        else:
+              aux = self.inicio
+              while aux != None:
+                print(aux.valor)
+                aux = aux.proximo
        
 l = Lista()
 l.adicionar(1)
