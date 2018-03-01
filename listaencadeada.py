726ff44a27390bbcf84f85b78764fdf8657d9a13
Luan Kleyton <36345746+kleytonluan@users.noreply.github.com>
2018-03-01 13:43:03 -0300

    Update listaencadeada.py

--- listaencadeada.py
+++ listaencadeada.py
@@ -23,10 +23,30 @@ class Lista:
               while aux != None:
                 print(aux.valor)
                 aux = aux.proximo
+                
+     def remover(self, item):
+        
+        atual = self.inicio
+        anterior = None
+        encontrado = False
+        
+        while not encontrado:
+            if atual.valor == item:
+                encontrado = True
+            else:
+                anterior = atual
+                atual = atual.proximo
+                
+        if anterior == None:
+            self.inicio = atual.proximo
+        else:
+            anterior.proximo = atual.proximo
        
 l = Lista()
 l.adicionar(1)
 l.adicionar(2)
 l.adicionar(3)
 l.adicionar(4)
+l.mostrar()
+l.remover(3)
 l.mostrar()
